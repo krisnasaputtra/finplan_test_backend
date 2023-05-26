5a50118d8e7bf3fcaecab67d0f3dd6e3fcea88f5
krisnasaputtra <krisna.saputra031002@gmail.com>
2023-05-26 23:07:23 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Test SQL Back End
+
+# Profile
+
+Nama : Krisna Saputra <br>
+Email : krisna.saputra031002@gmail.com
+
+# Project Description <a name="project-desc"></a>
+
+Cara menjalankan program:
+
+- Download file pada github repository atau bisa juga melalui git clone pada github repository
+- Setelah itu, pastikan aplikasi XAMPP sudah terinstall pada device anda
+- Lalu, Jalankan XAMPP
+- Buka phpMyAdmin pada browser favorite anda dengan mengetikkan localhost pada browser anda dan anda akan melihat tulisan 'phpMyAdmin' pada bagian navbar
+- Lalu buat database dengan nama database yang anda suka
+- lalu Jalankan query sql pada file <b>backend_test_1.sql</b> untuk membuat tabel beserta isinya
+- Lalu copy dan jalankan query sql pada file backend_test_2.txt, backend_test_3.txt, dan backend_test_4.txt
+
+# Screenshot Hasil <a name="result"></a>
+
+- Query SQL 1
+  ![]()
+- Query SQL 2
+  ![]()
+- Query SQL 3
+  ![]()
+- Query SQL 4
+  ![]()
---
+++ backend_test_1.sql
@@ -0,0 +1,136 @@
+-- Active: 1683526549326@@127.0.0.1@3306@finplan_test
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 25 Bulan Mei 2023 pada 17.21
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+--
+-- Database: `finplan_test`
+--
+
+-- --------------------------------------------------------
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeks untuk tabel `users`
+--
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `gender`, `status`) VALUES
+(1, 'Frasch', 'F', 'active'),
+(2, 'Garmuth', 'M', 'active'),
+(3, 'Goliath\r\n', 'M', 'active'),
+(4, 'Luna', 'F', 'active'),
+(5, 'Zeus', 'M', 'active'),
+(6, 'Aphrodite', 'F', 'active'),
+(7, 'Ares', 'M', 'active'),
+(8, 'Lina', 'F', 'active'),
+(9, 'Lanaya', 'F', 'active'),
+(10, 'Hades', 'M', 'active');
+
+
+-- --------------------------------------------------------
+--
+-- Struktur dari tabel `hobbies`
+--
+
+CREATE TABLE `hobbies` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeks untuk tabel `hobbies`
+--
+
+ALTER TABLE `hobbies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dumping data untuk tabel `hobbies`
+--
+
+INSERT INTO `hobbies` (`id`, `name`, `level`) VALUES
+(1, 'Running', 8),
+(2, 'Skipping', 5),
+(3, 'Push Up', 10);
+
+-- --------------------------------------------------------
+--
+-- Struktur dari tabel `map_user_hobby`
+--
+
+CREATE TABLE `map_user_hobby` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_hobby` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeks untuk tabel `map_user_hobby`
+--
+
+ALTER TABLE `map_user_hobby`
+  ADD PRIMARY KEY (`id`),
+  ADD INDEX (`id_user`),
+  ADD INDEX (`id_hobby`);
+
+--
+-- Dumping data untuk tabel `map_user_hobby`
+--
+
+INSERT INTO `map_user_hobby` (`id`, `id_user`, `id_hobby`, `status`) VALUES
+(1, 1, 1, 'active'),
+(2, 3, 1, 'active'),
+(3, 8, 3, 'deleted'),
+(4, 2, 2, 'active'),
+(5, 4, 1, 'deleted'),
+(6, 6, 2, 'active'),
+(7, 5, 3, 'active'),
+(8, 8, 1, 'active'),
+(9, 7, 2, 'active'),
+(10, 4, 2, 'active'),
+(11, 9, 3, 'deleted'),
+(12, 10, 2, 'deleted'),
+(13, 3, 2, 'active'),
+(14, 2, 3, 'active'),
+(15, 10, 2, 'active');
+
+
+-- --------------------------------------------------------
+--
+-- Ketidakleluasaan untuk tabel `map_user_hobby`
+--
+ALTER TABLE `map_user_hobby`
+  ADD CONSTRAINT `map_user_hobby_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `map_user_hobby_ibfk_2` FOREIGN KEY (`id_hobby`) REFERENCES `hobbies` (`id`);
+
+COMMIT;
+
+
